cdc0893d744b3ab92934e1691a08e3220eb49467
ebosetalee <princess01ella.miley@gmail.com>
2021-08-06 17:09:20 +0100

    patch: Formatting

--- utils/marsrover.js
+++ utils/marsrover.js
@@ -1,71 +1,70 @@
 class MarsRover {
-    constructor(position, direction){
-        this.position = position
-        this.direction = direction
-        this.x = position[0]
-        this.y = position[1]
-    }
+	constructor(position, direction) {
+		this.position = position;
+		this.direction = direction;
+		this.x = position[0];
+		this.y = position[1];
+	}
 
-    turnLeft(direction){
-        switch (direction.toUpperCase()){
-            case "NORTH":
-                return this.direction = "WEST"
-            case "WEST":
-                return this.direction = "SOUTH"
-            case "SOUTH":
-                return this.direction = "EAST"
-            case "EAST":
-                return this.direction = "NORTH"
-        }
-    }
+	turnLeft(direction) {
+		switch (direction.toUpperCase()) {
+			case "NORTH":
+				return (this.direction = "WEST");
+			case "WEST":
+				return (this.direction = "SOUTH");
+			case "SOUTH":
+				return (this.direction = "EAST");
+			case "EAST":
+				return (this.direction = "NORTH");
+		}
+	}
 
-    turnRight(direction){
-        switch (direction.toUpperCase()){
-            case "NORTH":
-                return this.direction = "EAST"
-            case "EAST":
-                return this.direction = "SOUTH"
-            case "SOUTH":
-                return this.direction = "WEST"
-            case "WEST":
-                return this.direction = "NORTH"
-        }
-    }
+	turnRight(direction) {
+		switch (direction.toUpperCase()) {
+			case "NORTH":
+				return (this.direction = "EAST");
+			case "EAST":
+				return (this.direction = "SOUTH");
+			case "SOUTH":
+				return (this.direction = "WEST");
+			case "WEST":
+				return (this.direction = "NORTH");
+		}
+	}
 
-    move(command){
-        if (command === "F"){
-            if (this.direction === "NORTH" || this.direction === "SOUTH") {
-                this.x += 1
-            } else if (this.direction === "EAST" || this.direction === "WEST") {
-                this.y += 1
-            }
-        } else if (command === "B"){
-            if (this.direction === "NORTH" || this.direction === "SOUTH") {
-                this.x -= 1
-            } else if (this.direction === "EAST" || this.direction === "WEST") {
-                this.y -= 1
-            }
-        } else{
-            return "Wrong command"
-        }
-    }
+	move(command) {
+		if (command === "F") {
+			if (this.direction === "NORTH" || this.direction === "SOUTH") {
+				this.x += 1;
+			} else if (this.direction === "EAST" || this.direction === "WEST") {
+				this.y += 1;
+			}
+		} else if (command === "B") {
+			if (this.direction === "NORTH" || this.direction === "SOUTH") {
+				this.x -= 1;
+			} else if (this.direction === "EAST" || this.direction === "WEST") {
+				this.y -= 1;
+			}
+		} else {
+			return "Wrong command";
+		}
+	}
 
-    translate(commands){
-        let x, eachCommand = commands.toUpperCase().split("")
-        for (x in eachCommand){
-            if (eachCommand[x] === "R"){
-                this.turnRight(this.direction)
-            } else if (eachCommand[x] === "L") {
-                this.turnLeft(this.direction)
-            } else{
-                this.move(eachCommand[x])
-            }
-        }
-        return (`[${this.x}, ${this.y}], ${this.direction}`)
-    }
-
+	translate(commands) {
+		let command = commands.toUpperCase().split(""), x;
+		for (x in command) {
+			if (command[x] === "R") {
+				this.turnRight(this.direction);
+			} else if (command[x] === "L") {
+				this.turnLeft(this.direction);
+			} else {
+				this.move(command[x]);
+			}
+		}
+		return `[${this.x}, ${this.y}], ${this.direction}`;
+	}
 }
 
-const mars = new MarsRover([4, 2], "EAST")
+const mars = new MarsRover([4, 2], "EAST");
 
-console.log(mars.translate("FLFFFRFLB"))
+console.log(mars.translate("FLFFFRFLB"));
